5db51a478590d37c0b5f8c642c75fd5e310a28eb
amlrr66 <amlrr66@hotmail.com>
2025-10-29 11:14:35 -0500

    tttttttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-// App.jsx
+import Chat from "./components/Chat";
+import Aura from "./components/aura";
 
-import { useState } from 'react'; // 1. Importa useState
-import Chat from './components/Chat'; // 
-import Aura from './components/aura'; //
+
 function App() {
-  // 
-  const [currentView, setCurrentView] = useState('chat');
-
   return (
     <div className="h-screen">
-      {/* 5. Renderiza un componente u otro segun el estado */}
-      {currentView === 'chat' ? (
-        // Pasa la función para cambiar de vista al componente Chat
-        <Chat setView={setCurrentView} />
-      ) : (
-        // Pasa la misma función al componente Aura para que pueda volver
-        <Aura setView={setCurrentView} />
-      )}
+      <Chat />
     </div>
   );
 }
