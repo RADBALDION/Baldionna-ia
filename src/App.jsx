62e623e45f611059e3f0d1f89cd96a095b4566bf
amlrr66 <amlrr66@hotmail.com>
2025-10-29 11:21:39 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Chat from "./components/Chat";
+import Aura from "./components/aura";
 
+export default function App() {
+  const [view, setView] = useState("chat"); // Vista inicial: Chat
 
-function App() {
   return (
-    <div className="h-screen">
-      <Chat />
+    <div className="app-container">
+      {view === "chat" && <Chat setView={setView} />}
+      {view === "aura" && <Aura setView={setView} />}
     </div>
   );
 }
-
-export default App;
